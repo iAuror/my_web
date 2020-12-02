e35dc7b29d5bef3f7b1750f4542441c6471480cb
iAuror <menestrellka@gmail.com>
2020-12-02 18:20:45 +0300

    add simple bootstrap layout

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 
-from news.models import News
+from news.models import News, Category
 
-admin.site.register (News)
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category','created_new','updated_new','published')
+    list_display_links = ('id','title')
+    search_fields = ('title','content')
+    list_filter = ('category', 'published')
+    list_editable = ('published',)
+
+
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('id','title')
+    list_display_links = ('id','title')
+    search_fields = ('title',)
+
+admin.site.register (News,NewsAdmin)
+admin.site.register (Category,CategoryAdmin)
--- news/models.py
+++ news/models.py
@@ -2,12 +2,29 @@ from django.db import models
 
 
 class News (models.Model):
-    title= models.CharField(max_length=200, verbose_name='title')
-    content =models.TextField()
-    created_new=models.DateTimeField(auto_now_add=True)
-    updated_new=models.DateTimeField(auto_now=True)
+    title= models.CharField(max_length=200, verbose_name='Оглавление')
+    content =models.TextField(verbose_name="Текст новости")
+    created_new=models.DateTimeField(auto_now_add=True, verbose_name="Время создания новости")
+    updated_new=models.DateTimeField(auto_now=True, verbose_name="Время последнего редактирования")
     photo=models.ImageField(upload_to='NewPhoto/%Y/%m/%d')
-    published=models.BooleanField (default=False)
+    published=models.BooleanField (default=False, verbose_name="Опубликованно")
+    category=models.ForeignKey('Category',on_delete=models.PROTECT, verbose_name='Категории',null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name='Новость'
+        verbose_name_plural='Новости'
+        ordering=['-created_new']
+
+class Category (models.Model):
+    title=models.CharField(max_length=200, verbose_name='Категории')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name='Категория'
+        verbose_name_plural='Категории'
+        ordering=['title']
